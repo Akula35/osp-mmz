dd748f533274c6ac8387bf2709515f3d7dfe2c0f
KamilW <kamilwojciechowski97@wp.pl>
2022-03-23 21:17:51 +0100

    nowa wersja z raportem

--- Server.js
+++ Server.js
@@ -16,11 +16,27 @@ app.get('/', (req, resp) => {
     const months = ["Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien"];
     var d = new Date()
     var dzis = d.getDate() + "." +months[d.getMonth()] + "." + d.getFullYear();
+    var dzisT = d.getFullYear()*10000+d.getMonth()*100+d.getDate();
     try{
         var tabela = fs.readFileSync('./tabela.json')
         app.result = JSON.parse(tabela);
     }catch(e){}
-    resp.render('index.ejs', {tabela: app.result, data: dzis, numer: ""});
+    var report=[0,0,0,0];
+    for(var i=0; i < app.result.length; i++){
+        if(dzisT == app.result[i].obiad){
+            report[0]++;
+        }
+        if(dzisT == app.result[i].zywnosc){
+            report[1]++;
+        }
+        if(dzisT == app.result[i].chemia){
+            report[2]++;
+        }
+        if(dzisT == app.result[i].inne){
+            report[3]++;
+        }
+    }
+    resp.render('index.ejs', {tabela: app.result, data: dzis, dataT: dzisT, numer: "", raport: report});
 })
 app.get('/wydaj', (req, resp) => {
     try{
@@ -29,27 +45,47 @@ app.get('/wydaj', (req, resp) => {
     }catch(e){}
     const months = ["Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien"];
     var d = new Date()
-    var dzis = d.getDate() + "." +months[d.getMonth()] + "." + d.getFullYear();
+    var dzis = d.getDate() + " " +months[d.getMonth()] + " " + d.getFullYear();
+    var dzisT = d.getFullYear()*10000+d.getMonth()*100+d.getDate();
+    var report=[0,0,0,0];
+    for(var i=0; i < tabelaJ.length; i++){
+        if(dzisT == tabelaJ[i].obiad){
+            report[0]++;
+        }
+        if(dzisT == tabelaJ[i].zywnosc){
+            report[1]++;
+        }
+        if(dzisT == tabelaJ[i].chemia){
+            report[2]++;
+        }
+        if(dzisT == tabelaJ[i].inne){
+            report[3]++;
+        }
+    }
     if(req.query.wydaj){
         for(var i=0; i < tabelaJ.length; i++){
             if(tabelaJ[i].numer == req.query.numer){
                 if(req.query.obiad){
-                    tabelaJ[i].obiad=dzis;
+                    tabelaJ[i].obiad=dzisT;
+                    report[0]++;
                 }
                 if(req.query.zywnosc){
-                    tabelaJ[i].zywnosc=dzis;
+                    tabelaJ[i].zywnosc=dzisT;
+                    report[1]++;
                 }
                 if(req.query.chemia){
-                    tabelaJ[i].chemia=dzis;
+                    tabelaJ[i].chemia=dzisT;
+                    report[2]++;
                 }
                 if(req.query.inne){
-                    tabelaJ[i].inne=dzis;
+                    tabelaJ[i].inne=dzisT;
+                    report[3]++;
                 }
             }
         }
         tabela = JSON.stringify(tabelaJ);
         fs.writeFileSync('./tabela.json', tabela);
-        resp.render('index.ejs', {tabela: tabelaJ, data: dzis, numer: req.query.numer});
+        resp.render('index.ejs', {tabela: tabelaJ, data: dzis, dataT: dzisT, numer: req.query.numer, raport: report});
     }
     if(req.query.szukaj){
         var tabelaS = [];
@@ -58,6 +94,6 @@ app.get('/wydaj', (req, resp) => {
                 tabelaS.push(tabelaJ[i]);
             }
         }
-        resp.render('index.ejs', {tabela: tabelaS, data: dzis, numer: req.query.numer});
+        resp.render('index.ejs', {tabela: tabelaS, data: dzis, dataT: dzisT, numer: req.query.numer, raport: report});
     }
 })
